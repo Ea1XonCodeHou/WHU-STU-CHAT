c7a5ece68436d1da112f473eefbecdc716453cf5
NotEles <1589921216@qq.com>
2025-04-26 16:44:28 +0800

    modify readme

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a0cf0c5524db26ee4584e0e983d087dd1b3e5b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b53555aee6c76e3cea3940d08d724400f544e14")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
